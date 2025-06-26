9d8b1e31e4a204fbe7d690a9180ab3f4d9ae0030
嶋田里奈 <shimadarina@shimadarinanoMacBook-Pro.local>
2025-06-26 17:20:30 +0900

    feat: DBにデータを保存する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Todo;
 
+
 class TodoController extends Controller
 {
     public function index()
@@ -20,5 +21,17 @@ class TodoController extends Controller
        // TODO: 第1引数を指定
         return view('todo.create'); // 追記
     }
+
+    public function store(Request $request) // 追記
+    {
+        $content = $request->input('content'); // 追記
+
+        $todo = new Todo();
+        $todo->content = $content;
+        $todo->save();
+
+        return redirect()->route('todo.index');
+    }
+
 
 }
